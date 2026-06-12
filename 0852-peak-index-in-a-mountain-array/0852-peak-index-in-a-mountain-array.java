bf42a9dcb93c8a8f90c9993cc25e19fc4a277bcd
MALATHI G <malathiganesan2006@gmail.com>
2026-06-12 19:05:58 +0530

    Time: 0 ms (100%), Space: 80.6 MB (7.55%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s < e) {
+
+            int mid =  (e + s) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+
+        return s;
+    }
+}
