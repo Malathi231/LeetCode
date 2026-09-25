b82b48bd6be268e2014fa39189574cf945ca22b8
MALATHI G <malathiganesan2006@gmail.com>
2026-09-25 19:08:02 +0530

    Time: 535 ms (94.18%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    ROUND(
+        COUNT(a2.player_id) * 1.0 / COUNT(a1.player_id),
+        2
+    ) AS fraction
+FROM
+    (SELECT player_id, MIN(event_date) AS first_date
+     FROM Activity
+     GROUP BY player_id) a1
+LEFT JOIN Activity a2
+    ON a1.player_id = a2.player_id
+    AND a2.event_date = DATE_ADD(a1.first_date, INTERVAL 1 DAY);
